bf5c38f3de0eecc406ccce4836a1f4690ebc096f
RJ Cunanan <rjlugtuc@gmail.com>
2021-03-02 17:14:06 -0800

    Added comments and removed unnecessary code.

--- loadingBarPageScripts.js
+++ loadingBarPageScripts.js
@@ -1,24 +1,23 @@
 let startLoading = false;
 let timeInterval = 100;
 
+// Creates the animation that the loading bar is filling up
+// Using a JavaScript timer:
 const startLoadingBar = () => {
-    if (startLoading == false) {
-        startLoading = true;
-        let currentProgress = 0;
-        let id = setInterval(loading, timeInterval);
-        function loading() {
-            if (currentProgress >= 100) {
-                clearInterval(id);
-                startLoading = false;
-            }
-            else {
-                currentProgress++;
-                document.getElementById("progress").style.width = currentProgress + "%";
-            }
+    let currentProgress = 0;
+    function loading() {
+        if (currentProgress >= 100) {
+            clearInterval(id);
+        }
+        else {
+            currentProgress++;
+            document.getElementById("progress").style.width = currentProgress + "%";
         }
     }
+    let id = setInterval(loading, timeInterval);
 }
 
+// Decreases the speed of the loading bar:
 const decreaseSpeed = () => {
     if (timeInterval >= 1000) {
         timeInterval = 1000;
@@ -28,6 +27,7 @@ const decreaseSpeed = () => {
     }
 }
 
+// Increases the speed of the loading bar:
 const increaseSpeed = () => {
     if (timeInterval <= 10) {
         timeInterval = 10;
